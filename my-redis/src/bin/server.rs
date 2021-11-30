9791dd7c8966d0d8c1f45a05bff40af1cc6c51e2
flyflyflypeng <flyflyflypeng@gmail.com>
2021-11-30 21:35:11 +0800

    add tokio tutorial examples before IO section
    Signed-off-by: flyflyflypeng <flyflyflypeng@gmail.com>

---
+++ my-redis/src/bin/server.rs
@@ -0,0 +1,57 @@
+use bytes::Bytes;
+use mini_redis::Command::{self, Get, Set};
+use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
+async fn process(socket: TcpStream, db: Db) {
+    // The `Connection` lets us read/write redis **frames** instead of
+    // byte streams. The `Connection` type is defined by mini-redis.
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening...");
+
+    // create global hashmap shared by different tasks
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        // The second item contains the IP and port of the new connection
+        let (socket, _) = listener.accept().await.unwrap();
+        // Clone the handle to the hashmap
+        let db = db.clone();
+
+        println!("accepted");
+
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
+    }
+}
